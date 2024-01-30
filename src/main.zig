c62519c3dd8d5f455c279efd010453d2632572e6
Ayman Lezhar <58956198+Haxeil@users.noreply.github.com>
2024-01-30 08:10:20 +0100

    camera movement

--- src/main.zig
+++ src/main.zig
@@ -9,15 +9,8 @@ pub fn main() !void {
     var engine = try Engine.Engine.init(1200, 800, "zeus-zig | frames: {0} , updates: {0}, delta: {0}");
     defer engine.deinit();
 
-    const v = .{
-        .left = -10,
-        .right = 10,
-        .bottom = -10,
-        .top = 10,
-        .near = -100,
-        .far = 10_000,
-    };
-    engine.camera.projection_matrix = math.Mat4x4.projection2D(v);
+    engine.camera.projection_matrix = math.Mat4x4.perspective(math.degreesToRadians(f32, 90), 1, -1.1, 10_000);
+    var cam_offset = math.vec3(0, 0, 10);
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
@@ -89,13 +82,30 @@ pub fn main() !void {
 
     var motion = math.vec3(0, 0, 0);
 
+    var speed: f32 = 2;
+
     while (engine.is_running()) {
+        if (engine.window.?.getKey(.w) == glfw.Action.press) {
+            cam_offset.v[2] -= 0.1 * engine.engine_time.?.delta * speed;
+        } else if (engine.window.?.getKey(.s) == glfw.Action.press) {
+            cam_offset.v[2] += 0.1 * engine.engine_time.?.delta * speed;
+        }
+
+        if (engine.window.?.getKey(.d) == glfw.Action.press) {
+            cam_offset.v[0] -= 0.1 * engine.engine_time.?.delta * speed;
+        } else if (engine.window.?.getKey(.a) == glfw.Action.press) {
+            cam_offset.v[0] += 0.1 * engine.engine_time.?.delta * speed;
+        }
+
+        const motion_matrix = math.Mat4x4.translate(cam_offset);
+        engine.camera.view_matrix = math.Mat4x4.ident.mul(&motion_matrix);
         shader.bind();
         // Engine.Shader.set_matrix(0, engine.camera.projection_matrix);
         const time: f32 = @floatCast(glfw.getTime());
         motion.v[0] = math.sin(time);
         Engine.Shader.set_vec3(0, motion);
         Engine.Shader.set_matrix(1, engine.camera.projection_matrix);
+        Engine.Shader.set_matrix(2, engine.camera.view_matrix);
 
         mesh.bind();
         mesh2.bind();
--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -3,6 +3,7 @@ layout (location = 0) in vec3 aPos;
 
 uniform vec3 _offset;
 uniform mat4 _p;
+uniform mat4 _v;
 
 
 void main()
@@ -10,5 +11,5 @@ void main()
     //gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
     vec3 p = aPos + _offset;
     
-    gl_Position = _p * vec4(p.x, p.y, p.z, 1.0);
+    gl_Position = _p * _v * vec4(p.x, p.y, p.z, 1.0);
 }
