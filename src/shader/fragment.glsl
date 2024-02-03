6bb0e3a16cee648f1d3c59dff73eee8ae17c824b
Ayman Lezhar <58956198+Haxeil@users.noreply.github.com>
2024-02-03 11:50:35 +0100

    added normal

--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -1,7 +1,9 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 out_normal;
+
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    FragColor = vec4(1.0f + out_normal.x, 0.5f + out_normal.y, 0.2f + out_normal.z, 1.0f);
 } 
