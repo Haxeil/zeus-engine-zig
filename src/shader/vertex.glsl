25074c8c7a2ad0d3a8613b3fb16041360337dc6d
Ayman Lezhar <58956198+Haxeil@users.noreply.github.com>
2024-02-03 11:51:23 +0100

    added normal

--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -1,5 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 uv;
+layout (location = 2) in vec3 normal;
+layout (location = 3) in vec4 color;
+ 
+out vec3 out_normal;
 
 uniform vec3 _offset;
 uniform mat4 _p;
@@ -8,7 +13,7 @@ uniform mat4 _v;
 
 void main()
 {
-    //gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
     vec3 p = aPos + _offset;
     gl_Position = _p * _v * vec4(p.x, p.y, p.z, 1.0);
+    out_normal = normal;
 }
